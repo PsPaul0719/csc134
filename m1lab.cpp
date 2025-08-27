39c228c9bb15e310d0e8ec6a797d3d0962206277
PsPaul0719 <spencep0788@student.faytechcc.edu>
2025-08-27 15:00:12 +0000

    m1lab

---
+++ m1lab.cpp
@@ -0,0 +1,20 @@
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string item_name = "apple";
+int  num_items = 10;
+double cost_per = 0.25;
+
+cout << "Welcome to the " << item_name << " store! " << endl;
+cout << "Each " << item_name << " is $" << cost_per <<endl;
+cout << "We have " << num_items << " total." << endl;
+
+double total_cost = num_items * cost_per;
+
+cout << "Buying them all wil cost: $ " << total_cost << endl;
+cout << "Thanks for shopping with us!" << endl;
+return 0;
+}
