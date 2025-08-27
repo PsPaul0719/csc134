23472614be0429b00b202741d9cbfaedeb91a1ae
PsPaul0719 <spencep0788@student.faytechcc.edu>
2025-08-27 15:44:40 +0000

    M1T1 Greetings

---
+++ m11t1.cpp
@@ -0,0 +1,23 @@
+
+/* 
+m1t1 - hello, user
+spencep, 8/27/25
+
+INPUT: run the program
+PROCESS: none yet
+OUTPUT: return 0 
+SPECIAL FX: Greet the user.
+*/
+
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+// SFX: Greet User
+string first_name = "Paul";
+
+cout << "Hello, " << first_name << "." << endl;
+
+return 0;
+}
