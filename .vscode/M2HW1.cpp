2ef6aaf0b67a99f0435f5a81aa6bfc51353d7f56
PsPaul0719 <spencep0788@student.faytechcc.edu>
2025-09-17 15:55:30 +0000

    m2hw1_void

--- .vscode/M2HW1.cpp
+++ .vscode/M2HW1.cpp
@@ -1,19 +1,37 @@
+/*
+CSC 134
+M2HW1
+Paul Spence
+Date: 
+*/
+
 #include <iostream>
 #include <iomanip>
 using namespace std; 
 
-void question1() {
 
-    cout << question1 << endl;
-
-}
-
-void question2() {
-
-    cout << question2 << endl;
-}
-
-int main(){
+int main() {
+double starting_bal;
+double deposit;
+double withdrawal;
+double fin_bal;
+double title;
 
 
+cout << "Please enter name:" << endl;
+cin >> title;
+cout << "Hello, " << title << ".";
+cout << "Enter your starting balence.";
+cin >> starting_bal;
+cout << "Enter your deposit:"; 
+cin >> deposit;
+cout << "Enter your widthdrawal:";
+cin >> withdrawal;
+
+double fin_bal = starting_bal + deposit - withdrawal;
+
+cout << setprecision(2) << fixed;
+cout << "Your final balence is" << fin_bal << endl;
+
+return 0;
 }
